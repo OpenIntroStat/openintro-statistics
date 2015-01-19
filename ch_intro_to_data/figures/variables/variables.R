f33fd018d94e8edfda9753baefac79778554d715
OpenIntroOrg <openintro.org@gmail.com>
2015-01-19 14:34:39 -0800

    Add 'nominal' variable definition

--- ch_intro_to_data/figures/variables/variables.R
+++ ch_intro_to_data/figures/variables/variables.R
@@ -3,7 +3,7 @@ data(COL)
 
 myPDF('variables.pdf', 4.2, 1.5, mar = rep(0,4))
 plot(c(-0.15, 1.3),
-     0:1,
+     c(0, 1),
      type = 'n',
      axes = FALSE)
 
@@ -26,12 +26,16 @@ text(0.39, 0.1, 'discrete')
 rect(0.25, 0, 0.53, 0.2)
 arrows(0.35, 0.38, 0.4, 0.22, length = 0.08)
 
-text(0.77, 0.105, 'regular\ncategorical', col = COL[6], cex = 0.7)
-rect(0.64, 0, 0.9, 0.2, border = COL[6])
+text(0.77, 0.14, 'nominal', col = COL[6], cex = 0.7)
+text(0.77, 0.05, '(unordered categorical)',
+     col = COL[6],
+     cex = 0.5)
+rect(0.6, 0, 0.94, 0.2, border = COL[6])
 arrows(0.82, 0.38, 0.77, 0.22, length = 0.08, col = COL[6])
 
-text(1.12, 0.1, 'ordinal', col = COL[6])
-rect(0.99, 0, 1.25, 0.2, border = COL[6])
-arrows(1.02, 0.38, 1.1, 0.22, length = 0.08, col = COL[6])
+text(1.14, 0.14, 'ordinal', col = COL[6], cex = 0.7)
+text(1.14, 0.05, '(ordered categorical)', col = COL[6], cex = 0.5)
+rect(0.98, 0, 1.3, 0.2, border = COL[6])
+arrows(1.03, 0.38, 1.11, 0.22, length = 0.08, col = COL[6])
 
 dev.off()
