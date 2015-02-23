7a606c977856e4b82cb691b5b1d484ae496a3ade
OpenIntroOrg <openintro.org@gmail.com>
2015-02-22 17:50:10 -0800

    Introduce t distribution first for numerical inference chapter

---
+++ ch_inference_for_means/figures/run10SampTimeHistogram/run10SampTimeHistogram.R
@@ -0,0 +1,15 @@
+library(openintro)
+data(COL)
+
+data(run10Samp)
+d <- run10Samp
+
+myPDF("run10SampTimeHistogram.pdf", 3.5, 2.8,
+      mar = c(3.5, 3.5, 0.5, 1),
+      mgp = c(2.2, 0.55, 0))
+histPlot(d$time,
+         main = "",
+         xlab = "Time (minutes)",
+         ylab = "Frequency",
+         col = COL[1])
+dev.off()
