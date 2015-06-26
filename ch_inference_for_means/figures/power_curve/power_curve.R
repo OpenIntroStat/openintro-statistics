bffd4a3c3504dd3ea23f257f6799d93ec27492df
OpenIntroOrg <openintro.org@gmail.com>
2015-06-25 22:31:59 -0700

    Revisions to the new Power section

---
+++ ch_inference_for_means/figures/power_curve/power_curve.R
@@ -0,0 +1,25 @@
+library(openintro)
+data(COL)
+
+n <- c(10:500, seq(510, 2000, 10), seq(2100, 10000, 100))
+se <- sapply(n, function(x) sqrt(2 * 12^2 / x))
+left.reject <- -1.96 * se
+x <- (left.reject - (-3)) / se
+p <- pt(x, 2 * n - 2)
+
+myPDF('power_curve_neg-3.pdf', 7, 3)
+plot(n, p,
+     xlab = "Sample Size Per Group",
+     ylab = "Power",
+     xlim = c(20, 5000),
+     ylim = 0:1,
+     type = "n",
+     log = "x",
+     axes = FALSE)
+axis(1)
+axis(2)
+abline(h = 0:1, lty = 2, col = COL[6,2])
+lines(n, p, col = COL[1], lwd = 3)
+dev.off()
+
+
