73a97874e9b67abd9f654da427d190f1dcd13951
OpenIntroOrg <openintro.org@gmail.com>
2015-06-27 21:30:49 -0700

    Further formatting to Inference for Numerical Data chapter text

--- ch_inference_for_means/figures/stemCellTherapyForHearts/stemCellTherapyForHearts.R
+++ ch_inference_for_means/figures/stemCellTherapyForHearts/stemCellTherapyForHearts.R
@@ -6,9 +6,9 @@ d <- stem.cells
 change <- d$after - d$before
 t.test(change ~ d[,1])
 
-myPDF('stemCellTherapyForHearts.pdf', 4, 5,
-      mar=c(4, 1.8, 3, 0.7),
-      mgp=c(2, 0.45, 0),
+myPDF('stemCellTherapyForHearts.pdf', 4, 3.5,
+      mar=c(3.2, 1.8, 1.7, 0.7),
+      mgp=c(2, 0.3, 0),
       mfrow=c(2, 1))
 histPlot(change[d[,1] == 'esc'],
          xlim=c(-10, 15),
@@ -21,13 +21,13 @@ x.axis.at <- seq(-10, 15, 5)
 x.axis.labels <- paste0(seq(-10, 15, 5), "%")
 cex.axis <- 0.85
 axis(1, x.axis.at, x.axis.labels, cex.axis=cex.axis)
-mtext('Embryonic stem cell transplant', line=1.5, cex=1.25)
-mtext('Change in heart pumping function', 1, line=1.5, cex=0.9)
+mtext('Embryonic stem cell transplant', line=0.5, cex=1.1)
+mtext('Change in heart pumping function', 1, line=1.3, cex = 0.9)
 par(mgp=c(2, 0.6, 0))
 axis(2, at=0:3, cex.axis=0.925)
 
-par(mar=c(2.6, 1.8, 4, 0.7),
-    mgp=c(2, 0.5, 0))
+par(mar=c(2.4, 1.8, 2, 0.7),
+    mgp=c(2, 0.3, 0))
 histPlot(change[d[,1] == 'ctrl'],
          xlim=c(-10, 15),
          axes=FALSE,
@@ -38,6 +38,6 @@ histPlot(change[d[,1] == 'ctrl'],
 axis(1, x.axis.at, x.axis.labels, cex.axis=cex.axis)
 par(mgp=c(2, 0.6, 0))
 axis(2, at=0:3, cex.axis=0.925)
-mtext('Control (no treatment)', line=1.5, cex=1.25)
-mtext('Change in heart pumping function', 1, line=1.5, cex=0.9)
+mtext('Control (no treatment)', line=0.5, cex=1.1)
+mtext('Change in heart pumping function', 1, line=1.3, cex = 0.9)
 dev.off()
--- ch_inference_for_means/figures/textbooksS10/diffInTextbookPricesS10.R
+++ ch_inference_for_means/figures/textbooksS10/diffInTextbookPricesS10.R
@@ -4,7 +4,7 @@ data(COL)
 
 d <- textbooks
 
-myPDF('diffInTextbookPricesS10.pdf', 5.4, 3.2,
+myPDF('diffInTextbookPricesS10.pdf', 6, 3,
       mar = c(3, 3.2, 0.5, 0.5),
       mgp = c(1.8, 0.5, 0))
 histPlot(d$diff, axes = FALSE,
@@ -13,6 +13,6 @@ histPlot(d$diff, axes = FALSE,
          ylab = "",
          col = COL[1])
 mtext("Frequency", 2, 2.1, las = 0)
-axis(1, cex.axis = 0.87, tck = -0.03)
-axis(2, at = seq(0, 30, 10), cex.axis = 0.87, tck = -0.02)
+axis(1, tck = -0.03)
+axis(2, at = seq(0, 30, 10), tck = -0.02)
 dev.off()
