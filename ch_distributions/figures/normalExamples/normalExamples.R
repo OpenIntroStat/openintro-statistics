f208c507d5e4d2174f8061a44e2c30e23a3c15b9
OpenIntroOrg <openintro.org@gmail.com>
2016-01-31 22:08:00 -0800

    Minor updates and typo fixes

--- ch_distributions/figures/normalExamples/normalExamples.R
+++ ch_distributions/figures/normalExamples/normalExamples.R
@@ -4,7 +4,7 @@ data(COL)
 source("normalExamples-data.R")
 source("normalExamples-helpers.R")
 
-myPDF("normalExamples.pdf", 6.6, 4,
+myPDF("normalExamples.pdf", 7.3, 4.4,
       mfrow = c(2, 3),
       mgp = c(2, 0.7, 0),
       mar = c(3, 0, 1, 1))
@@ -12,7 +12,7 @@ NormalHist(obs1, hold1, M1, SD1, COL[1])
 NormalHist(obs2, hold2, M2, SD2, COL[2])
 NormalHist(obs3, hold3, M3, SD3, COL[3])
 
-par(mar = c(3,2.85,1,2))
+par(mar = c(3,2.85,1,1.8))
 QQNorm(obs1, M1, SD1, COL[1])
 QQNorm(obs2, M2, SD2, COL[2])
 QQNorm(obs3, M3, SD3, "#B09A00")
--- ch_inference_for_means/figures/textbooksS10/diffInTextbookPricesS10.R
+++ ch_inference_for_means/figures/textbooksS10/diffInTextbookPricesS10.R
@@ -7,7 +7,7 @@ d <- textbooks
 myPDF('diffInTextbookPricesS10.pdf', 6, 3,
       mar = c(3, 3.2, 0.5, 0.5),
       mgp = c(1.8, 0.5, 0))
-histPlot(d$diff, axes = FALSE,
+histPlot(d$diff, axes = FALSE, # breaks = 20,
          xlim = c(-20, 80),
          xlab = "UCLA price - Amazon price (USD)",
          ylab = "",
@@ -15,4 +15,5 @@ histPlot(d$diff, axes = FALSE,
 mtext("Frequency", 2, 2.1, las = 0)
 axis(1, tck = -0.03)
 axis(2, at = seq(0, 30, 10), tck = -0.02)
+# axis(2, at = seq(0, 15, 5), tck = -0.02)
 dev.off()
