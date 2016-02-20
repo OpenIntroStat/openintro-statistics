ef4a12a1778530cb94bb96f66e4c390115ddd339
OpenIntroOrg <openintro.org@gmail.com>
2016-02-20 15:15:04 -0800

    Make a margin labels a little smaller in two plots

--- ch_inference_foundations/figures/eoce/adult_heights/adult_heights.R
+++ ch_inference_foundations/figures/eoce/adult_heights/adult_heights.R
@@ -6,7 +6,7 @@ data(bdims)
 
 # histogram of heights ----------------------------------------------
 
-pdf("adult_heights_hist.pdf", height = 3, width = 6)
-par(mar=c(3.7,2.5,0.5,0.5), las=1, mgp=c(2.5,0.7,0), cex.lab = 1.5)
+myPDF("adult_heights_hist.pdf", 6, 3,
+    mar = c(3.2,2.2,0.5,0.5), las=1, mgp=c(2,0.55,0))
 histPlot(bdims$hgt, col = COL[1], xlab = "Height", ylab = "")
 dev.off()
--- ch_inference_foundations/figures/eoce/college_credits/college_credits.R
+++ ch_inference_foundations/figures/eoce/college_credits/college_credits.R
@@ -6,7 +6,7 @@ data(credits)
 
 # histogram of college credits --------------------------------------
 
-pdf("college_credits_hist.pdf", height = 3, width = 6)
-par(mar=c(3.7,2.2,0.5,0.5), las=1, mgp=c(2.5,0.7,0), cex.lab = 1.5)
+myPDF("college_credits_hist.pdf", 6, 3,
+    mar = c(3.2,2.2,0.5,0.5), las=1, mgp=c(2,0.55,0))
 histPlot(credits[,1], col = COL[1], xlab = "Number of credits", ylab = "")
 dev.off()
