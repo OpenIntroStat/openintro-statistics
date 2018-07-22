2b0c77389dc6519182d9a474dd6717058d216bd4
David Diez <david@openintro.org>
2018-07-22 10:14:27 -0700

    Clean up the two new sections for inference introduction for proportions, which corresponds to moving to new survey data about renewable energy and also being more consistent in discussing the proportion context rather than a more generic context.

---
+++ ch_inference_for_props/figures/sampling_10k_prop_887p/sampling_10k_prop_887p.R
@@ -0,0 +1,30 @@
+set.seed(3)
+library(openintro)
+data(COL)
+
+n.sim <- 10000
+samp.size <- 1000  # 2541
+prop <- 0.887
+
+samples <- matrix(sample(0:1, n.sim * samp.size, TRUE, c(1 - prop, prop)), n.sim)
+results <- apply(samples, 1, mean)
+mean(results)
+sd(results)
+
+myPDF('sampling_10k_prop_887p.pdf', 6.5, 3.2,
+    mar = c(3.5, 3.5, 0.7, 0.7),
+    mgp = c(2.3, 0.6, 0),
+    yaxs = "i")
+histPlot(results,
+    col = COL[1], breaks = 25,
+    xlab = "Sample Proportions",
+    ylab = "",
+    axes = FALSE)
+at <- seq(0, 1, 0.02)
+axis(1, at = seq(0, 1, 0.01), labels = rep("", 101))
+axis(1, at = at)
+# axis(2, at = seq(0, 1200, 100), label = rep("", 13))
+axis(2, at = seq(0, 1500, 500))
+# abline(v = 0.89, col = COL[4])
+
+dev.off()
