2591cb4bad0274725d99c9470cbb635ed286ffd8
David Diez <david@openintro.org>
2018-09-09 17:35:09 -0700

    Revise (generally) HT section in FFI chapter before sending for first review

--- ch_foundations_for_inf/figures/normal_dist_mean_500_se_016/normal_dist_mean_500_se_016.R
+++ ch_foundations_for_inf/figures/normal_dist_mean_500_se_016/normal_dist_mean_500_se_016.R
@@ -4,24 +4,25 @@ data(COL)
 fn1 <- 'normal_dist_mean_500_se_016.pdf'
 fn2 <- 'normal_dist_mean_500_se_016_with_upper.pdf'
 
-GenerateGraph <- function(show.upper = FALSE) {
+GenerateGraph <- function(show.tails = FALSE) {
   normTail(0.5, 0.016, L = 0.37, U = 0.63, col = COL[1],
       xlim = c(0.32, 0.68), axes = FALSE)
   at <- c(-1, 0.37, 0.5, 0.63, 2)
   font.36 <- 1
-  if (!show.upper) {
+  if (!show.tails) {
     at <- c(-1, 0.37, 0.5, 2)
     font.36 <- 2
   }
   axis(1, at, cex.axis = 0.9)
-  lines(c(-1, 0.37), rep(0, 2), lwd = 5, col = COL[1])
-  arrows(0.37, 7, 0.35, 1,
-         length = 0.1,
-         lwd = 2,
-         col = COL[1])
-  expr <- expression("Tail Area for "*hat(p))
-  text(0.39, 7, "Tail Area for 0.37", pos = 3, col = COL[1], font = font.36)
-  if (show.upper) {
+  if (show.tails) {
+    lines(c(-1, 0.37), rep(0, 2), lwd = 5, col = COL[1])
+    arrows(0.37, 7, 0.35, 1,
+           length = 0.1,
+           lwd = 2,
+           col = COL[1])
+    expr <- expression("Tail Area for "*hat(p))
+    text(0.39, 7, expr, pos = 3, col = COL[1],
+        font = font.36)
     lines(c(1, 0.63), rep(0, 2), lwd = 5, col = COL[1])
     arrows(0.63, 7, 0.65, 1,
            length = 0.1,
@@ -29,6 +30,14 @@ GenerateGraph <- function(show.upper = FALSE) {
            col = COL[1])
     expr <- expression("Equally unlikely if "*H[0]*" is true")
     text(0.61, 7, expr, pos = 3, col = COL[1], cex = 0.8)
+  } else {
+    arrows(0.38, 7, 0.371, 1,
+           length = 0.1,
+           lwd = 2,
+           col = COL[1])
+    expr <- expression("Observed "*hat(p)*" = 0.37")
+    text(0.39, 7, expr, pos = 3, col = COL[1],
+        font = font.36)
   }
 }
 
