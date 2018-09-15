c51085a851b6493d00e905967f76c75d0743281c
David Diez <david@openintro.org>
2018-09-15 14:56:52 -0700

    Tweak description of when to use diferent model selection options. Update Cherry Blossom Run data to use results from 2017.

--- ch_inference_for_means/figures/run10SampTimeHistogram/run10SampTimeHistogram.R
+++ ch_inference_for_means/figures/run10SampTimeHistogram/run10SampTimeHistogram.R
@@ -1,6 +1,7 @@
 library(openintro)
 data(COL)
 
+
 data(run10Samp)
 d <- run10Samp
 
@@ -13,3 +14,22 @@ histPlot(d$time,
          ylab = "Frequency",
          col = COL[1])
 dev.off()
+
+
+set.seed(1)
+run17 <- subset(run17, event == "10 Mile")
+mean(run17$net_sec / 60)
+d <- run17[sample(nrow(run17), 100), ]
+myPDF("run17SampTimeHistogram.pdf", 5, 2.8,
+      mar = c(3.5, 3.5, 0.5, 1),
+      mgp = c(2.2, 0.55, 0))
+histPlot(d$net_sec / 60,
+         main = "",
+         xlab = "Time (minutes)",
+         ylab = "Frequency",
+         col = COL[1])
+dev.off()
+t.test(d$net_sec / 60, mu = 93.29)
+mean(d$net_sec / 60)
+sd(d$net_sec / 60)
+
