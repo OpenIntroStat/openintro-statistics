d150c121bd13446500ba305c4d9b8280038c9452
David Diez <david@openintro.org>
2018-11-07 21:19:56 -0800

    Updates to county data set in Ch 1 and updating intro to 2.1

---
+++ ch_intro_to_data/figures/medianHHIncomePoverty/medianHHIncomePoverty.R
@@ -0,0 +1,40 @@
+library(openintro)
+data(county)
+data(COL)
+
+ind <- 1088
+
+myPDF("medianHHIncomePoverty.pdf", 6, 3.5,
+      mar = c(3, 4.7, 0.5, 1),
+      mgp = c(2.4, 0.5, 0))
+x <- county$poverty
+y <- county$median_hh_income
+plot(x, y,
+     pch = 20,
+     cex = 0.7,
+     col = COL[1, 3],
+     xlim = c(0, max(x, na.rm = TRUE)),
+     ylim = c(0, max(y, na.rm = TRUE)),
+     xlab = "",
+     ylab = "",
+     axes = FALSE)
+AxisInPercent(1, pretty(c(0, x)))
+AxisInDollars(2, pretty(c(0, y)))
+box()
+points(x, y, pch = ".")
+mtext("Poverty Rate (Percent)", 1, 1.9)
+par(las = 0)
+mtext("Median Household Income", 2, 3.5)
+t1 <- x[ind]
+t2 <- y[ind]
+lines(c(t1, t1), c(-1e5, t2),
+      lty = 2,
+      col = COL[4])
+lines(c(-1e5, t1), c(t2, t2),
+      lty = 2,
+      col = COL[4])
+points(t1, t2,
+       col = COL[4])
+dev.off()
+
+county[ind, ]
--- ch_intro_to_data/figures/multiunitsVsOwnership/multiunitsVsOwnership.R
+++ ch_intro_to_data/figures/multiunitsVsOwnership/multiunitsVsOwnership.R
@@ -1,6 +1,4 @@
 library(openintro)
-data(countyComplete)
-cc <- countyComplete
 data(COL)
 
 w3  <- 1 == 0
@@ -17,28 +15,30 @@ if(w3){
         mgp = c(2.4, 0.4, 0))
 }
 pch    <- 1
-cex    <- sqrt(cc$pop2017 / 1e6)
+cex    <- sqrt(county$pop2017 / 1e6)
+cex[is.na(cex)] <- 0.1
 colPop <- fadeColor(ifelse(cex > 0.35, COL[4], COL[1]),
                     substr(gray(0.6 + cex * 0.1), 2, 3))
 colSm  <- colPop
 cexF   <- 2
+gp1 <- cex < 0.32
 if(!w3){
   cex <- 0.7
+  gp1 <- rep(TRUE, nrow(county))
   pch <- 20
-  gp1 <- cex < 0.32
   colSm  <- COL[1, 3]
   colPop <- COL[1, 3]
   cexF   <- 1
 }
-x <- cc$housing_multi_unit
-y <- cc$home_ownership
+x <- county$multi_unit
+y <- county$homeownership
 plot(x[gp1], y[gp1],
      pch = pch,
      col = colSm,
      xlab = "",
-     ylab = "Percent of Homeownership",
+     ylab = "Homeownership Rate",
      axes = FALSE,
-     cex = ifelse(cex < 0.32, 0.32, cex)[gp1],
+     cex = ifelse(gp1 & cex < 0.32, 0.32, cex)[gp1],
      xlim = c(0, 100), # range(x, na.rm = TRUE),
      ylim = range(y, na.rm = TRUE))
 at  =  seq(0, 100, 20)
@@ -46,14 +46,15 @@ axis(1, at, paste0(at, "%"))
 axis(2, at, paste0(at, "%"))
 abline(h = at, v = at, col = COL[7, 2])
 box()
+points(x[gp1],
+       y[gp1],
+       pch = '.')
 points(x[!gp1], y[!gp1],
        pch = pch,
        col = colPop,
-       xlab = "",
-       ylab = "Percent of Homeownership",
        cex = ifelse(cex < 0.32, 0.32, cex)[!gp1])
-points(cc$housing_multi_unit[!gp1],
-       cc$home_ownership[!gp1],
+points(x[!gp1],
+       y[!gp1],
        pch = '.')
 t1 <- x[ind]
 t2 <- y[ind]
