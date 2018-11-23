bb71c4df86d91c57b41f1ecb56aa95ba495eeadc
David Diez <david@openintro.org>
2018-11-23 15:23:04 -0800

    Update textbook data in paired data section for a new textbook data set.

---
+++ ch_inference_for_means/figures/textbooksF18/diffInTextbookPricesF18.R
@@ -0,0 +1,19 @@
+library(openintro)
+data(textbooks)
+data(COL)
+
+d <- as.numeric(na.omit(ucla_textbooks_f18$bookstore_new -
+    ucla_textbooks_f18$amazon_new))
+
+myPDF('diffInTextbookPricesF18.pdf', 6, 3,
+      mar = c(3, 3.2, 0.5, 0.5),
+      mgp = c(1.8, 0.5, 0))
+histPlot(d, axes = FALSE, # breaks = 20,
+         xlim = c(-20, 80),
+         xlab = "UCLA Bookstore Price - Amazon Price (USD)",
+         ylab = "",
+         col = COL[1])
+AxisInDollars(1, at = pretty(d), tck = -0.03)
+axis(2, at = seq(0, 15, 5), tck = -0.02)
+# axis(2, at = seq(0, 15, 5), tck = -0.02)
+dev.off()
---
+++ ch_inference_for_means/figures/textbooksF18/textbooksF18HTTails.R
@@ -0,0 +1,25 @@
+library(openintro)
+data(textbooks)
+data(COL)
+d <- as.numeric(na.omit(ucla_textbooks_f18$bookstore_new -
+    ucla_textbooks_f18$amazon_new))
+m <- mean(d)
+s <- sd(d)
+se <- s / sqrt(length(d))
+z <- m / se
+
+myPDF('textbooksF18HTTails.pdf', 5, 1.6,
+      mar = c(1.7, 0, 0, 0),
+      mgp = c(3, 0.5, 0))
+normTail(L = -abs(m),
+         U = abs(m),
+         s = se,
+         df = 20,
+         # xlim = 5 * c(-1, 1),
+         col = COL[1],
+         # border = COL[4],
+         axes = FALSE)
+at <- c(-100, 0, m, 100)
+labels <- expression(0, mu[0]*' = 0', bar(x)[diff]*" = 2.98", 0)
+axis(1, at, labels, cex.axis = 0.9)
+dev.off()
