413dfc9c95f3a6402cb6daaac9837e68eb45132e
David Diez <david@openintro.org>
2019-01-10 20:51:24 -0800

    Polish section on summarizing categorical data

--- ch_summarizing_data/figures/loan_app_type_home_seg_bar/loan_app_type_home_seg_bar.R
+++ ch_summarizing_data/figures/loan_app_type_home_seg_bar/loan_app_type_home_seg_bar.R
@@ -8,7 +8,7 @@ cp <- prop.table(tab, 2)
 
 myPDF("loan_app_type_home_seg_bar.pdf",
     4.5, 3.5,
-    mar = c(2, 3, 0.5, 0.5),
+    mar = c(2, 4, 0.5, 0.5),
     mgp = c(2.2, 0.6, 0))
 ylim <- c(0, max(apply(tab, 1, sum)))
 barplot(apply(tab, 1, sum),
@@ -24,11 +24,13 @@ abline(h = 0)
 legend("topright",
     fill = COL[c(3,1)],
     legend = c("joint", "individual"))
+par(las = 0)
+mtext("Count", 2, 2.9)
 dev.off()
 
 myPDF("loan_app_type_home_sbs_bar.pdf",
     4.5, 3.5,
-    mar = c(2, 3, 0.5, 0.5),
+    mar = c(2, 4, 0.5, 0.5),
     mgp = c(2.2, 0.6, 0))
 barplot(t(tab),
     ylim = ylim,
@@ -37,11 +39,13 @@ abline(h = 0)
 legend("topright",
     fill = COL[c(3,1)],
     legend = c("joint", "individual"))
+par(las = 0)
+mtext("Count", 2, 2.9)
 dev.off()
 
 myPDF("loan_app_type_home_seg_bar_standardized.pdf",
-    4.5, 3.5,
-    mar = c(2, 2.5, 0.5, 0.5),
+    5, 3.5,
+    mar = c(2, 4, 0.5, 0.5),
     mgp = c(2.2, 0.6, 0))
 barplot(apply(tab, 1, sum) / apply(tab, 1, sum), col = COL[3])
 tabTemp <- rp[, 1]
@@ -50,6 +54,12 @@ barplot(tabTemp,
     col = COL[1],
     add = TRUE,
     axes = FALSE)
+legend(2.65, 0.3,
+    fill = COL[c(3,1)],
+    legend = c("joint", "individual"),
+    bg = "white")
 abline(h = 0)
+par(las = 0)
+mtext("Proportion", 2, 2.9)
 dev.off()
 
