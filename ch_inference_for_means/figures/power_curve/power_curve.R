ff3c4e06d6de2b5ee913dc858af151e2bedbcb6a
David Diez <david@openintro.org>
2019-03-02 21:13:13 -0800

    Remove last core usages of probability tables, and also switch power section to (implicitly) use the t-distribution

--- ch_inference_for_means/figures/power_curve/power_curve.R
+++ ch_inference_for_means/figures/power_curve/power_curve.R
@@ -3,7 +3,7 @@ data(COL)
 
 n <- c(10:500, seq(510, 2000, 10), seq(2100, 10000, 100))
 se <- sapply(n, function(x) sqrt(2 * 12^2 / x))
-left.reject <- -1.96 * se
+left.reject <- qt(0.025, n - 1) * se
 x <- (left.reject - (-3)) / se
 p <- pt(x, 2 * n - 2)
 
