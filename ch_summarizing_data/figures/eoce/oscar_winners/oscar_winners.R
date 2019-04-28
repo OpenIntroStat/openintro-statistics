c0a55617bd139fd39ce27806ac46041049307819
David Diez <david@openintro.org>
2019-04-28 10:08:29 -0700

    Make a plot in an exercise easier to read (font was too small)

--- ch_summarizing_data/figures/eoce/oscar_winners/oscar_winners.R
+++ ch_summarizing_data/figures/eoce/oscar_winners/oscar_winners.R
@@ -1,20 +1,20 @@
 # load packages -----------------------------------------------------
 library(openintro)
+library(forcats)
 
 # load data ---------------------------------------------------------
 data(oscars)
 
 # plot of oscar winner women and men ages ---------------------------
-p <- oscars %>%
+myPDF("oscars_winners_hist.pdf", 5, 3.15)
+oscars %>%
   ggplot(aes(x = age)) +
     geom_histogram(binwidth = 10, fill = COL[1,1], color = COL[5,1], size = 0.3) +
     facet_wrap(~fct_rev(award), ncol = 1) +
     theme_minimal() +
     theme(strip.text = element_text(hjust = 0)) +
     labs(x = "Age (in years)", y = "")
-
-ggsave(p, file = "ch_intro_to_data/oscar_winners/figures/oscars_winners_hist.pdf",
-       height = 6, width = 8)
+dev.off()
 
 # summary stats -----------------------------------------------------
 oscars %>%
