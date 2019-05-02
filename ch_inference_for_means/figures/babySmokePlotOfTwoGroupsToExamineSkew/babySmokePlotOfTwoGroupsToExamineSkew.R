e4afbeb29f4248009ac26039889cdebbb2531eaa
David Diez <david@openintro.org>
2019-05-01 20:23:47 -0700

    Fix typos / tweaks from Mine's feedback. Do some modified formatting in data appendix.

--- ch_inference_for_means/figures/babySmokePlotOfTwoGroupsToExamineSkew/babySmokePlotOfTwoGroupsToExamineSkew.R
+++ ch_inference_for_means/figures/babySmokePlotOfTwoGroupsToExamineSkew/babySmokePlotOfTwoGroupsToExamineSkew.R
@@ -6,13 +6,14 @@ d <- births
 
 myPDF('babySmokePlotOfTwoGroupsToExamineSkew.pdf', 2 * 4.5, 2.3,
       mfrow = 1:2, #2:1,
-      mar = c(3, 1, 0, 1),
+      mar = c(3, 1, 2.5, 1),
       mgp = c(1.7, 0.55, 0))
-xlab.start <- 'Newborn Weights (lbs) From Mothers Who'
+xlab.start <- 'Newborn Weights (lbs)'
 histPlot(d$weight[d$smoke == 'smoker'],
          xlim = c(0, 11),
          axes = FALSE,
-         xlab = paste(xlab.start, 'Smoked'),
+         xlab = xlab.start,
+         main = 'Mothers Who Smoked',
          col = COL[1])
 axis(1)
 
@@ -20,7 +21,8 @@ axis(1)
 histPlot(d$weight[d$smoke == 'nonsmoker'],
          xlim = c(0, 11),
          axes = FALSE,
-         xlab = paste(xlab.start, 'Did Not Smoke'),
+         xlab = xlab.start,
+         main = 'Mothers Who Did Not Smoke',
          col = COL[1])
 axis(1)
 dev.off()
