80a772f0fa291c4d928543300a82d5964414b1e3
David Diez <david@openintro.org>
2022-09-24 18:30:44 -0500

    Add alt text for Chapter 4 and also for all figures that appear in the odd-numbered exercise solutions appendix

--- ch_probability/figures/eoce/tree_hiv_swaziland/tree_hiv_swaziland.R
+++ ch_probability/figures/eoce/tree_hiv_swaziland/tree_hiv_swaziland.R
@@ -2,7 +2,7 @@
 library(openintro)
 
 # tree --------------------------------------------------------------
-pdf("tree_hiv_swaziland.pdf", width = 6, height = 2.5)
+pdf("tree_hiv_swaziland.pdf", width = 7, height = 2.5)
 treeDiag(c("HIV?", "Result"), 
          c(0.259, 1-0.259), list(c(0.997, 0.003), c(1-0.926, 0.926)), 
          c("yes","no"), c("positive","negative"), 
