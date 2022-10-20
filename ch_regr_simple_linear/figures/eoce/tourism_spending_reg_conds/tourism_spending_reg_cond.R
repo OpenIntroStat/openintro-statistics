085095785a87ab8fcd4fa5b387d3c31c45a4d9a0
David Diez <david@openintro.org>
2022-10-19 21:54:30 -0500

    Add alt-text for Chapter 8, and tweak a few plots

--- ch_regr_simple_linear/figures/eoce/tourism_spending_reg_conds/tourism_spending_reg_cond.R
+++ ch_regr_simple_linear/figures/eoce/tourism_spending_reg_conds/tourism_spending_reg_cond.R
@@ -1,28 +1,36 @@
-# load packages -----------------------------------------------------
+rm(list = ls())
 library(openintro)
 
-# load data ---------------------------------------------------------
+tourism$visitor_count <- 1e3 * tourism$visitor_count_tho
+tourism$tourist_spending <- 1e6 * tourism$tourist_spending
 
-tourism_spending <- read.csv("tourism_spending.csv")
+m_spending_count <- lm(tourist_spending ~ visitor_count,
+                       data = tourism)
 
-# model spending vs. count ------------------------------------------
-
-m_spending_count <- lm(tourist_spending ~ visitor_count_thousand,
-                       data = tourism_spending)
 
 # plot spending vs. count -------------------------------------------
 
-pdf("tourism_spending_count.pdf", 5.5, 4.3)
+myPDF(
+  "tourism_spending_count.pdf",
+  5.5, 4.3,
+  mar = c(3.5, 5.5, 1, 0.5),
+  mgp = c(2.5, 0.7, 0), 
+  cex.lab = 1.5,
+  cex.axis = 1.5
+)
 
-par(mar = c(3.5, 4, 1, 0.5), las = 0, mgp = c(2.5, 0.7, 0), 
-    cex.lab = 1.5, cex.axis = 1.5)
-
-plot(tourist_spending ~ visitor_count_thousand,
-     data = tourism_spending, 
+plot(tourist_spending ~ visitor_count,
+     data = tourism, 
      col = COL[1,2], 
-     xlab = "Number of tourists (thousands)",
-     ylab = "Spending (million $)", 
-     pch = 19)
+     xlab = "Number of Tourists",
+     ylab = "",
+     pch = 19,
+     axes = FALSE)
+at <- seq(0, 25e6, 5e6)
+axis(1, at = at, labels = paste0(at / 1e6, "m"))
+AxisInDollars(2, at = seq(0, 15e9, 5e9))
+par(las = 0)
+mtext("Spending", 2, 4.2, cex = 1.5)
 
 abline(m_spending_count, col = COL[2], lwd = 2)
 
@@ -30,18 +38,26 @@ dev.off()
 
 # plot residuals ----------------------------------------------------
 
-pdf("tourism_spending_count_residuals.pdf", 5.5, 4.3)
+myPDF(
+  "tourism_spending_count_residuals.pdf",
+  5.5, 4.3,
+  mar = c(3.5, 5.5, 1, 0.5),
+  mgp = c(2.5, 0.7, 0), 
+  cex.lab = 1.5,
+  cex.axis = 1.5
+)
 
-par(mar = c(3.5, 4, 1, 0.5), las = 0, mgp = c(2.5, 0.7, 0), 
-    cex.lab = 1.5, cex.axis = 1.5)
-
-plot(m_spending_count$residuals ~ tourism_spending$visitor_count_thousand,
-     xlab = "Number of tourists (thousands)", ylab = "Residuals", 
-     col = COL[1,2], pch = 19,
-     ylim = c(-1600, 1600), axes = FALSE)
-axis(1)
-axis(2, at = seq(-1000, 1000, 1000))
-box()
+plot(
+  tourism$visitor_count,
+  m_spending_count$residuals,
+  xlab = "Number of Tourists",
+  ylab = "Residuals", 
+  col = COL[1,2], pch = 19,
+  ylim = c(-1600e6, 1600e6), axes = FALSE
+)
+at <- seq(0, 25e6, 5e6)
+axis(1, at = at, labels = paste0(at / 1e6, "m"))
+axis(2, at = seq(-1e9, 1e9, 1e9), labels = c("-$1b", "$0", "$1b"))
 
 abline(h = 0, lty = 2)
 
@@ -49,16 +65,21 @@ dev.off()
 
 # histogram of residuals --------------------------------------------
 
-pdf("tourism_spending_count_residuals_hist.pdf", 5.5, 4.3)
-
-par(mar = c(3.5, 2.5, 0.5, 0.5), las = 1, mgp = c(2.5, 0.7, 0), 
-    cex.lab = 1.5, cex.axis = 1.5)
+myPDF(
+  "tourism_spending_count_residuals_hist.pdf",
+  5.5, 4.3,
+  mar = c(3.7, 4, 1, 0.5),
+  mgp = c(2.5, 0.7, 0), 
+  cex.lab = 1.5,
+  cex.axis = 1.5
+)
 
 hist(m_spending_count$residuals,
      col = COL[1], 
-     xlab = "Residuals", ylab = "", main = "", 
+     xlab = "Residuals", ylab = "Count", main = "", 
      axes = FALSE, ylim = c(0,20))
-axis(1, at = seq(-1500, 1500, 750))
+axis(1, at = seq(-2e9, 2e9, 1e9), labels = c("-$2b", "-$1b", "$0", "$1b", "$2b"))
 axis(2, c(0, 10, 20))
+abline(h = 0)
 
 dev.off()
